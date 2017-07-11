a6a4ca063a8b8a0bcdd25e712c30bc00fa03373c
Valiceemo <richiewallace83@gmail.com>
2017-07-11 16:21:25 +0100

    Update gpu-mem-split.sh
    add appending of date to backup made

--- memory-split/gpu-mem-split.sh
+++ memory-split/gpu-mem-split.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
+NOW=$(date +"%y_%m_%d")
+
 { if 
 (whiptail --title "Memory Split" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to set the gpu memory split." 10 80) 
 then
-pass
+echo "Cancelled"
 else
 
 MEM_SPLIT=$(whiptail --inputbox "Enter amount of memory to assign to the GPU - for headless go with 16." 10 80 "16" 3>&1 1>&2 2>&3)
-sudo cp /boot/config.txt /home/pi/backups/boot/
+sudo cp /boot/config.txt /home/pi/backups/boot/config_$NOW.bk
 sudo sed -i '/gpu_mem/ d' /boot/config.txt
-sudo echo "gpu_mem=$NEWMEM_SPLIT" | sudo tee --append /boot/config.txt
+sudo echo "gpu_mem=$MEM_SPLIT" | sudo tee --append /boot/config.txt
 
 ## End of install
 fi }
