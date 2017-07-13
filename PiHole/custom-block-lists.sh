cb6f1492812a93920c590c2a77114978a290fd02
Valiceemo <richiewallace83@gmail.com>
2017-07-13 16:15:41 +0100

    Update custom-block-lists.sh
    added sleep for easier views,

--- PiHole/custom-block-lists.sh
+++ PiHole/custom-block-lists.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-echo "Adding lists to /etc/pihole/adlists.list..."
+VERSION=001
 
+echo "Adding lists to /etc/pihole/adlists.list..."
+sleep 1
 sudo echo "## HenningVanRaumle Youtube Ads" | sudo tee --append /etc/pihole/adlists.list
 sudo echo "https://raw.githubusercontent.com/HenningVanRaumle/pihole-ytadblock/master/ytadblock.txt" | sudo tee --append /etc/pihole/adlists.list
-
+sleep 1
 sudo echo "## DeathByBandAid's piholeparser list" | sudo tee --append /etc/pihole/adlists.list
 sudo echo "https://raw.githubusercontent.com/deathbybandaid/piholeparser/master/parsedall/1111ALLPARSEDLISTS1111edited.txt" | sudo tee --append /etc/pihole/adlists.list
 
 ## run pihole gravity update
 echo "Running piholegravity..."
+sleep 3
 sudo bash /etc/.pihole/gravity.sh
-
+sleep 2
 echo "...all done..."
+echo "Gathering Pi-Hole stats..."
+sleep 2
 sudo /home/pi/scripts/system/network/pihole-stats.sh
