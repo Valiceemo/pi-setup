0ec58931b703877fb71dc70b8b25e8791139cb2f
Valiceemo <richiewallace83@gmail.com>
2018-09-11 15:09:13 +0000

    Update custom-block-lists.sh
    Version 6
    Tidied up code, pi-hole gravity output is now seen

--- PiHole/custom-block-lists.sh
+++ PiHole/custom-block-lists.sh
@@ -1,56 +1,51 @@
 #!/bin/sh
 
-VERSION=005
+VERSION=006
 
 echo "================================================================"
 echo "=====                Valiceemo adlists.list                ====="
 echo "=====                   Credit to Wally3K                  ====="
 echo "================================================================"
 
-sleep 1
-
+sleep 0.5
+echo
+echo "Script written by Valiceemo https://github.com/Valiceemo"
+echo "All credit to Wally3K"
+echo "The script is a simple means to add the lists compiled & managed by Wally3K to your own Pi-Hole install"
 echo "This list will take the lists from https://wally3k.github.io/ and add them to your pi-hole install"
 sleep 3
 echo "Creating backup copy of adlists.list ==> /etc/pihole/adlists.list.bk"
+sleep 0.5
+sudo cp /etc/pihole/adlists.list /etc/pihole/adlists.list.bk
+sleep 0.5
+echo
+echo "Done, backup is at /etc/pihole/adlists.list.bk"
+echo
+sleep 1
 DOMAINS_BLOCKED_BEFORE=`curl --silent "http://localhost/admin/api.php?summary"| jq '.domains_being_blocked'`
 sleep 0.5
 
-sudo cp /etc/pihole/adlists.list /etc/pihole/adlists.list.bk
-echo "DONE"
-sleep 2
+## Add Wally3K lists to Pi-hole, default is type=nocross, modify at own will / risk
 echo "Adding lists to /etc/pihole/adlists.list..."
-echo "## Adlists modified by manunal script" | sudo tee --append /etc/pihole/adlists.list
-sudo curl -s https://v.firebog.net/hosts/lists.php?type=nocross | sudo tee --append /etc/pihole/adlists.list 
+echo | sudo tee --append /etc/pihole/adlists.list
+echo "#### Adlists modified by manunal script @ $(date)" | sudo tee --append /etc/pihole/adlists.list
+echo | sudo tee --append /etc/pihole/adlists.list
+curl -s https://v.firebog.net/hosts/lists.php?type=nocross | sudo tee --append /etc/pihole/adlists.list 
 sleep 2
-
-echo "DONE"
-
+echo "Done."
+echo
+sleep 1
 ## run pihole gravity update
 echo "Starting pihole gravity...Please be patient"
+echo "......"
 sleep 0.2
-sudo bash /etc/.pihole/gravity.sh >/dev/null 2>&1 &
-pid=$! # Process Id of the previous running command
-spin[0]="-"
-spin[1]="\\"
-spin[2]="|"
-spin[3]="/"
-echo -n "[Gravitating] ${spin[0]}"
-while [ kill -0 $pid ]
-do  
-  for i in "${spin[@]}" 
-  do        
-        echo -ne "\b$i"        
-        sleep 0.1  
-  done
-done
-
+sudo bash /etc/.pihole/gravity.sh
+echo
 echo "Completed, Pihole Gravity has been run with the added lists"
-
+sleep 1
 DOMAINS_BLOCKED_AFTER=`curl --silent "http://localhost/admin/api.php?summary"| jq '.domains_being_blocked'`
+echo
 echo "Domains being blocked before update:" $DOMAINS_BLOCKED_BEFORE
+echo
 echo "Domains being blocked after update:" $DOMAINS_BLOCKED_AFTER
 sleep 0.5
-
-echo "Gathering detailed Pi-Hole stats..."
-sleep 2
-sudo /home/pi/scripts/system/network/pihole-stats.sh
