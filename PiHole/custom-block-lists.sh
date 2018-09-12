95372a6e73811ff0e5a0d15aa5f20fe3a627084c
Valiceemo <richiewallace83@gmail.com>
2018-09-12 14:02:38 +0000

    Update custom-block-lists.sh
    V8
    added colouring

--- PiHole/custom-block-lists.sh
+++ PiHole/custom-block-lists.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
-VERSION=007
+VERSION=008
+
+# COLORS
+blackText=$(tput setaf 0)   # Black
+redText=$(tput setaf 1)     # Red
+greenText=$(tput setaf 2)   # Green
+yellowText=$(tput setaf 3)  # Yellow
+blueText=$(tput setaf 4)    # Blue
+magentaText=$(tput setaf 5) # Magenta
+cyanText=$(tput setaf 6)    # Cyan
+whiteText=$(tput setaf 7)   # White
+resetText=$(tput sgr0)      # Reset to default color
+
+# RESULTS
+TICK="[${greenText}✓${resetText}]"
+INFO="[i]"
+DONE="${greenText}  done!${resetText}"
+
+
 clear
 sleep 0.2
 echo "================================================================"
@@ -17,40 +35,46 @@ echo
 echo "This list will take the lists from https://wally3k.github.io/ and add them to your pi-hole install"
 echo
 sleep 3
-echo "Creating backup copy of adlists.list ==> /etc/pihole/adlists.list.bk"
+str="Creating backup copy of adlists.list ==> /etc/pihole/adlists.list.bk"
+echo "  ${INFO} ${str}..."
 sleep 0.5
 sudo cp /etc/pihole/adlists.list /etc/pihole/adlists.list.bk
 sleep 0.5
-echo
-echo "[✓]Done, backup is at /etc/pihole/adlists.list.bk"
-echo
+echo "  ${DONE}"
+str="backup is at /etc/pihole/adlists.list.bk"
+echo "  ${INFO} ${str}"
 sleep 1
 DOMAINS_BLOCKED_BEFORE=`curl --silent "http://localhost/admin/api.php?summary"| jq '.domains_being_blocked'`
 sleep 0.5
 
 ## Add Wally3K lists to Pi-hole, default is type=nocross, modify at own will / risk
-echo "Adding lists to /etc/pihole/adlists.list..."
+str="Adding lists to /etc/pihole/adlists.list"
+echo "  ${INFO} ${str}..."
 echo | sudo tee --append /etc/pihole/adlists.list >/dev/null 2>&1
 echo "#### Adlists modified by manunal script @ $(date)" | sudo tee --append /etc/pihole/adlists.list >/dev/null 2>&1
 echo | sudo tee --append /etc/pihole/adlists.list >/dev/null 2>&1
 curl --silent https://v.firebog.net/hosts/lists.php?type=nocross | sudo tee --append /etc/pihole/adlists.list >/dev/null 2>&1
 sleep 2
-echo "[✓] Done"
+echo "  ${DONE}"
 echo
 sleep 1
 ## run pihole gravity update
-echo "Starting pihole gravity...Please be patient"
-echo "......"
+str="Starting pihole gravity...Please be patient"
+echo "  ${INFO} ${str}..."
+
 sleep 0.2
 sudo bash /etc/.pihole/gravity.sh
 echo
-echo "[✓] Completed, Pihole Gravity has been run with the added lists"
+echo "  ${TICK} Completed, Pihole Gravity has been run with the added lists"
 echo
-echo "Checking No of blocked domains..."
-sleep 3
+str="Checking No of blocked domains"
+echo "  ${INFO} ${str}..."
+
+sleep 8
 DOMAINS_BLOCKED_AFTER=`curl --silent "http://localhost/admin/api.php?summary"| jq '.domains_being_blocked'`
 echo
-echo "Domains being blocked before update:" $DOMAINS_BLOCKED_BEFORE
+str="Domains being blocked before update:" $DOMAINS_BLOCKED_BEFORE
+echo "  ${INFO} ${str}{DOMANS_BBLOCKED_BEFORE"
 echo
 echo "Domains being blocked after update:" $DOMAINS_BLOCKED_AFTER
 sleep 0.5
