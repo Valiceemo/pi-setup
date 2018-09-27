83ffa3be5651139e11aa2c05b1bcf31e6e2a67dd
Valiceemo <richiewallace83@gmail.com>
2018-09-27 13:57:04 +0000

    Create parser.sh

---
+++ PiHole/parser.sh
@@ -0,0 +1,44 @@
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"https://easylist-downloads.adblockplus.org/easylist.txt > easylist.unsorted
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+
+echo -e " ${TICK} \e[32m All clean!  \e[0m"https://easylist-downloads.adblockplus.org/easyprivacy.txt > easyprivacy.unsorted
+echo -e " ${TICK} \e[32m All clean!  \e[0m"https://easylist-downloads.adblockplus.org/malwaredomains_full.txt > easymalware.unsorted
+
+
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+
+
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
+
+echo -e " ${TICK} \e[32m All clean!  \e[0m"
